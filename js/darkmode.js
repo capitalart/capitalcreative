268d9fd00ffa38bab69ed251b3cb268a902fd232
capitalart <robincustance@gmail.com>
2024-09-14 19:48:30 +0930

    Updated website content and structure

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,35 +1,17 @@
-// Icons for sun and moon
-const sunIcon = '<img src="icons/sun.svg" alt="Light Mode">';
-const moonIcon = '<img src="icons/moon.svg" alt="Dark Mode">';
+// Toggle dark/light mode
+const toggleThemeBtn = document.querySelector('.toggle-theme');
 
-// Set up dark/light toggle buttons
-const toggleThemeBtn = document.querySelectorAll('.toggle-theme');
-
-// Toggle theme function
 function toggleTheme() {
     const currentTheme = document.documentElement.getAttribute('data-theme');
     const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
     document.documentElement.setAttribute('data-theme', newTheme);
     localStorage.setItem('theme', newTheme);
-
-    // Update icons
-    toggleThemeBtn.forEach(button => {
-        button.innerHTML = newTheme === 'dark' ? sunIcon : moonIcon;
-    });
 }
 
-// Set initial theme
+// Set initial theme based on localStorage
 window.onload = () => {
     const storedTheme = localStorage.getItem('theme') || 'light';
     document.documentElement.setAttribute('data-theme', storedTheme);
 
-    // Set initial icons
-    toggleThemeBtn.forEach(button => {
-        button.innerHTML = storedTheme === 'dark' ? sunIcon : moonIcon;
-    });
-
-    // Add event listener to toggle buttons
-    toggleThemeBtn.forEach(button => {
-        button.addEventListener('click', toggleTheme);
-    });
+    toggleThemeBtn.addEventListener('click', toggleTheme);
 };
